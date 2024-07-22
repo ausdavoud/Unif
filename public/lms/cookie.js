1c3b020ea24e0c1915361213b0e0f25f75f26c46
Davoud Nosrati <davwvod@gmail.com>
2024-07-23 00:05:07 +0330

    Rename getCookie file to cookie

--- public/lms/cookie.js
+++ public/lms/cookie.js
@@ -11,26 +11,27 @@ const requests_1 = require("./requests");
 function getCookie(username, password) {
     const axiosInstance = axios_1.default.create();
     const payload = qs_1.default.stringify({
-        'username': username,
-        'password': password
+        username: username,
+        password: password,
     });
     let config = {
-        method: 'post',
+        method: "post",
         maxRedirects: 0,
         maxBodyLength: Infinity,
-        url: 'http://lms.ui.ac.ir/login',
+        url: "http://lms.ui.ac.ir/login",
         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
+            "Content-Type": "application/x-www-form-urlencoded",
         },
-        data: payload
+        data: payload,
     };
-    const cookie = axiosInstance.request(config)
-        .then(res => {
+    const cookie = axiosInstance
+        .request(config)
+        .then((res) => {
         throw new Error();
     })
-        .catch(err => {
+        .catch((err) => {
         if (err.response && err.response.status == 302) {
-            return err.response.headers['set-cookie'][1];
+            return err.response.headers["set-cookie"][1];
         }
         console.error(`Error in getting the cookie. Probably the status code was 
             not 302, meaning no redirect happened after Login.`);
@@ -38,12 +39,11 @@ function getCookie(username, password) {
     });
     return cookie;
 }
-;
 function isCookieValid(cookie) {
-    const URL = 'http://lms.ui.ac.ir/members/home';
+    const URL = "http://lms.ui.ac.ir/members/home";
     const validity = (0, requests_1.getURL)(URL, cookie)
-        .then(res => true)
-        .catch(err => {
+        .then((res) => true)
+        .catch((err) => {
         if (err.response.status == 302) {
             console.error(`Error in validating cookie. Home page redirects
             to another page which is probably the login page, so the cookie
@@ -56,14 +56,3 @@ function isCookieValid(cookie) {
     });
     return validity;
 }
-function testLogin() {
-    const username = process.env.lmsUsername || '';
-    const password = process.env.lmsPassword || '';
-    console.log('username', username);
-    console.log('password', password);
-    const cookie = getCookie(username, password)
-        .then(isCookieValid)
-        .then(console.log)
-        .catch(console.log);
-}
-// testLogin()
